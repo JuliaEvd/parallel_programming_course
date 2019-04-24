2308975faa30514e66f51ee57da20b865aaa7ef3
JuliaEvd <julia15evdokimova15@gmail.com>
2019-04-24 23:40:04 +0300

    task2

--- modules/task_2/evdokimova_j_gradient_task2_omp/main.cpp
+++ modules/task_2/evdokimova_j_gradient_task2_omp/main.cpp
@@ -104,11 +104,12 @@ void SoprGradMethod(double* matrix, double* vector, double* x0,
 }
 // omp
 void MatrixVector_omp(double* matrix, double* vector, double* result, int size) {
-	int i;
+    int i;
 #pragma omp parallel for private(i, j)
     for (i = 0; i < size; i++) {
         result[i] = 0;
-        for (int j = 0; j < size; j++) {
+        int j;
+        for (j = 0; j < size; j++) {
             result[i] += matrix[i * size + j] * vector[j];
         }
     }
